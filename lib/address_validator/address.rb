67b60685b163e6e91418dccb321faa4c3810583b
robhurring <robhurring@gmail.com>
2014-02-03 12:52:19 -0500

    add classifications for address

--- lib/address_validator/address.rb
+++ lib/address_validator/address.rb
@@ -2,15 +2,52 @@ require 'builder'
 
 module AddressValidator
   class Address
-    attr_accessor :name, :street1, :city, :state, :zip, :country
+    CLASSIFICATION_UNKNOWN = 0
+    CLASSIFICATION_COMMERCIAL = 1
+    CLASSIFICATION_RESIDENTIAL = 2
 
-    def initialize(name: name, street1: street1, city: city, state: state, zip: zip, country: country)
+    class << self
+      # Public: Build an address from the API's response xml.
+      #
+      # attrs  - Hash of address attributes.
+      #
+      # Returns an address.
+      def from_xml(attrs = {})
+        classification = nil
+
+        if _classification = attrs['AddressClassification']
+          classification = _classification['Code']
+        end
+
+        new(
+          street1: attrs['AddressLine'],
+          city: attrs['PoliticalDivision2'],
+          state: attrs['PoliticalDivision1'],
+          zip: attrs['PostcodePrimaryLow'],
+          country: attrs['CountryCode'],
+          classification: classification
+        )
+      end
+    end
+
+    attr_accessor :name, :street1, :city, :state, :zip, :country, :classification
+
+    def initialize(name: name, street1: street1, city: city, state: state, zip: zip, country: country, classification: classification)
       @name = name
       @street1 = street1
       @city = city
       @state = state
       @zip = zip
       @country = country
+      @classification = classification || CLASSIFICATION_UNKNOWN
+    end
+
+    def residential?
+      classification == CLASSIFICATION_RESIDENTIAL
+    end
+
+    def commercial?
+      classification == CLASSIFICATION_COMMERCIAL
     end
 
     def to_xml(options={})
