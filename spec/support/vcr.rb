b6bfceaef2a3ac87149f41b7cbf44947e4c21f85
robhurring <robhurring@gmail.com>
2014-02-20 12:03:11 -0500

    re-record settings

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,11 @@
 VCR.configure do |c|
+  single_day = 60*60*24
+
   c.cassette_library_dir = 'spec/data/vcr_cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
   c.configure_rspec_metadata!
+  c.default_cassette_options = {record: :new_episodes, re_record_interval: single_day}
   c.filter_sensitive_data('%{AccessLicenseNumber}'){ API_CONFIG['access_key'] }
   c.filter_sensitive_data('%{UserId}'){ API_CONFIG['username'] }
   c.filter_sensitive_data('%{Password}'){ API_CONFIG['password'] }
