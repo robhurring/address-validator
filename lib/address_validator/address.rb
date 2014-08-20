e7e6766a2d97ac1fce7175fc155733c5e82c0fc2
Brady Somerville <brady.somerville@gmail.com>
2014-08-20 16:53:13 -0500

    Support a second address line

--- lib/address_validator/address.rb
+++ lib/address_validator/address.rb
@@ -19,8 +19,17 @@ module AddressValidator
           classification = _classification['Code']
         end
 
+        street1, street2 = nil, nil
+        if attrs['AddressLine'].kind_of?(Array)
+          street1 = attrs['AddressLine'][0]
+          street2 = attrs['AddressLine'][1]
+        else
+          street1 = attrs['AddressLine']
+        end
+
         new(
-          street1: attrs['AddressLine'],
+          street1: street1,
+          street2: street2,
           city: attrs['PoliticalDivision2'],
           state: attrs['PoliticalDivision1'],
           zip: attrs['PostcodePrimaryLow'],
@@ -31,11 +40,12 @@ module AddressValidator
       end
     end
 
-    attr_accessor :name, :street1, :city, :state, :zip, :zip_extended, :country, :classification
+    attr_accessor :name, :street1, :street2, :city, :state, :zip, :zip_extended, :country, :classification
 
-    def initialize(name: name, street1: street1, city: city, state: state, zip: zip, zip_extended: zip_extended, country: country, classification: classification)
+    def initialize(name: name, street1: street1, street2: street2, city: city, state: state, zip: zip, zip_extended: zip_extended, country: country, classification: classification)
       @name = name
       @street1 = street1
+      @street2 = street2
       @city = city
       @state = state
       @zip = zip
@@ -57,7 +67,7 @@ module AddressValidator
 
       xml.AddressKeyFormat do
         xml.ConsigneeName(self.name)
-        xml.AddressLine(self.street1)
+        xml.AddressLine([self.street1, self.street2].join(' '))
         xml.PoliticalDivision2(self.city)
         xml.PoliticalDivision1(self.state)
         xml.PostcodePrimaryLow(self.zip)
